cea10f265be1b134473ed40907e3226b1d73fe33
Shakeel Sidhu <shakeeljutt070@gmail.com>
2022-01-21 18:09:24 +0500

    Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Login } from "./component/UI/RegEmail/Login/Login";
+import { CreateNear } from "./component/UI/RegEmail/CreateNearAccount/CreateNearAccount";
+import { Verification } from "./component/UI/RegEmail/Verification/Verification";
 import './App.css'
-
+import { BrowserRouter, Route,Routes } from "react-router-dom";
 function App() {
   return (
-      <div>
-        <Login/>
-      </div>
+      <BrowserRouter>
+              <Routes>
+                  <Route path='/' element={<Login/>}/>
+                  <Route path='/createnear' element={<CreateNear />}/>
+                  <Route path='/verification' element={<Verification />}/>
+              </Routes>
+      </BrowserRouter>
   );
 }
 
